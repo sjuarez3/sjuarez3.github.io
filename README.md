7845b8376d6474b94dd7f2144f1ea2fddacfea4c
Steve Juarez <sjuarez3@uwo.ca>
2017-03-29 17:07:14 -0400

    added subjects dropdown function and started course filtering

--- README.md
+++ README.md
@@ -4,7 +4,11 @@ GitHub page hosting our UWO Student Center Redesign Prototype
 with a real time FireBase database.
 
 Steve Juarez
+
 Jaisen Bynoe
+
 Johnny Zapras
+
 Yenan Jiang
+
 David Tsui
--- index.js
+++ index.js
@@ -1,5 +1,42 @@
-// Sets up shortcuts to Firebase features and initiate firebase auth.
-program-planning-tool.prototype.initFirebase = function() {
-  // Shortcuts to Firebase SDK features.
-  this.database = firebase.database();
-};
+// database reference
+var database = firebase.database();
+
+// subjects reference
+var subjects = database.ref('subjects');
+
+// courses reference
+var courses = database.ref('courses');
+
+// subjects dropdown menu
+subjects.on('value', function(snapshot) {
+	snapshot.forEach(function(snap) {
+
+		var subject = snap.val().subject;
+		var sel = document.getElementById('subjectSelect');
+		var opt = document.createElement('option');
+		opt.innerHTML = subject;
+		opt.value = subject;
+		sel.appendChild(opt);
+	});
+});
+
+// list courses
+function listCourses() {
+    var selectedCourse = document.getElementById("subjectSelect").value;	
+
+	courses.on('value', function(snapshot) {
+		snapshot.forEach(function(snap) {
+
+			var course = snap.val().subject;
+			var ul = document.getElementById('courselist');
+			li = ul.getElementsByTagName('li');
+			if(course == selectedCourse)
+			{
+				var opt = document.createElement('li');
+				opt.innerHTML = course;
+				opt.value = course;
+				ul.appendChild(opt);
+			}
+		});
+	});
+}
