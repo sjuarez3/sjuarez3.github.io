663f825936894461944966a2a706d8dfcfc1157a
Steve Juarez <sjuarez3@uwo.ca>
2017-04-01 10:22:16 -0400

    changed percentages to pixels to fix element position, changed formatting of search bars, add other minor css improvements

--- src/JS/firebaseFunctions.js
+++ src/JS/firebaseFunctions.js
@@ -383,3 +383,19 @@ function filterByMandatoryCourses() {
 		}
 	}
 }
+
+var selector, elems, makeActive;
+
+selector = '.filterLinks';
+
+elems = document.querySelectorAll(selector);
+
+makeActive = function () {
+    for (var i = 0; i < elems.length; i++)
+        elems[i].classList.remove('active');
+    
+    this.classList.add('active');
+};
+
+for (var i = 0; i < elems.length; i++)
+    elems[i].addEventListener('mousedown', makeActive);
